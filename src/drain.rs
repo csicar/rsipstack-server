ab198c30927f75551a422318d39956368962e2cb
Carsten Csiky <carsten.csiky@vier.ai>
2026-06-23 11:14:37 +0200

    document `DrainToken`

--- src/drain.rs
+++ src/drain.rs
@@ -1,5 +1,10 @@
 use tokio_util::sync::{CancellationToken, WaitForCancellationFuture};
 
+/// [DrainToken] is part of the mechanism that ensures rsipstack-server instances can
+/// be drained (e.g. for downtime-free service upgrades).
+/// [crate::server::SipServer] uses this to decide if `OPTIONS` request should be answered:
+/// After [DrainToken::start_drain] is called, [crate::server::SipServer] will no longer respond to
+/// `OPTIONS` requests, signaling to an upstream Proxy that no new calls should be send to this service instance.
 #[derive(Default, Clone)]
 pub struct DrainToken(CancellationToken);
 
